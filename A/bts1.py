0282ed22f68fdd749be5625c9b8fcb3f95eb7078
Hillway-git <2271379860@qq.com>
2019-12-20 17:45:11 +0800

    keyikeyi

--- A/bts1.py
+++ A/bts1.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+# def Foo(x):
+# #     if (x==1):
+# #         return 1
+# #     else:
+# #         return x+Foo(x-1)
+# #
+# # print(Foo(4))
+if None:
+    print('Hello')
---
+++ B/A/bts12.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# test_list = [1, 6, 3, 5, 3, 4]
+#
+# print("查看 4 是否在列表中 ( 使用循环 ) : ")
+#
+# for i in test_list:
+#     if (i == 4):
+#         print("存在")
+#
+# print("查看 4 是否在列表中 ( 使用 in 关键字 ) : ")
+#
+# if (4 in test_list):
+#     print("存在")
+#
+from bisect import bisect_left
+
+# 初始化列表
+test_list_set = [1, 6, 3, 5, 3, 4]
+test_list_bisect = [1, 6, 3, 5, 3, 4]
+
+print("查看 4 是否在列表中 ( 使用 set() + in) : ")
+
+test_list_set = set(test_list_set)
+if 4 in test_list_set:
+    print("存在")
+
+print("查看 4 是否在列表中 ( 使用 sort() + bisect_left() ) : ")
+
+test_list_bisect.sort()
+if bisect_left(test_list_bisect, 4):
+    print("存在")
+
